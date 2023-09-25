41531e68fbafb4a319d8e331b8fa729fcaff06c8
pramodyasahan <sahandil1013@gmail.com>
2023-09-25 22:55:37 +0530

    Finishing the Project

--- main.py
+++ main.py
@@ -19,3 +19,5 @@ regression = LinearRegression()
 regression.fit(X_train, y_train)
 y_pred = regression.predict(X_test)
 
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
