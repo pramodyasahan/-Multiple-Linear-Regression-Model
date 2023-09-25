65779499a2c0eb4ac0835d55fed87c7170de6fd5
pramodyasahan <sahandil1013@gmail.com>
2023-09-25 22:54:43 +0530

    Split data into training and test sets

--- main.py
+++ main.py
@@ -13,3 +13,6 @@ y = df.iloc[:, -1].values
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
