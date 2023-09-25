ec76cb9aed37299866d503fa5ea9399c791139f9
pramodyasahan <sahandil1013@gmail.com>
2023-09-25 22:55:08 +0530

    Creating the model

--- main.py
+++ main.py
@@ -15,4 +15,7 @@ X = np.array(ct.fit_transform(X))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+regression = LinearRegression()
+regression.fit(X_train, y_train)
+y_pred = regression.predict(X_test)
 
